ea57c65faa88d155e8bf998b39a72c42cde380e7
Juan Lopez <juan.lbarrera@mail.escuelaing.edu.co>
2026-04-09 11:05:18 -0500

    4  prueba del CI

--- src/test/java/edu/eci/dosw/tdd/DoswLibraryApplicationTests.java
+++ src/test/java/edu/eci/dosw/tdd/DoswLibraryApplicationTests.java
@@ -3,13 +3,17 @@ package edu.eci.dosw.tdd;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @ActiveProfiles("mongo")
+@TestPropertySource(properties = {
+		"spring.data.mongodb.uri=mongodb://localhost:27017/library-test",
+		"spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration,org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration"
+})
 class DoswLibraryApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
